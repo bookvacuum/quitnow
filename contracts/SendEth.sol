5a07237a816e82a239d8748c561effcfcc129718
michellewong793 <michellewong793@gmail.com>
2022-11-04 20:06:11 -0700

    add contract address where the thing is deployed

--- contracts/SendEth.sol
+++ contracts/SendEth.sol
@@ -4,7 +4,7 @@
 pragma solidity ^0.8.0;
 
 // Switch this to your own contract address once deployed, for bookkeeping!
-// Example Contract Address on Goerli: 0xDBa03676a2fBb6711CB652beF5B7416A53c1421D
+// Example Contract Address on Goerli: 0x898221a7eeC77A5F0102CBfa74969615e69f52cc
 
 contract SendEth {
     // Event to emit when a Memo is created.
